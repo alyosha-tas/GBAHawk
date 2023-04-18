924766393d6b17c5381090a34b2b4d45f0d21235
alyosha-tas <alexei.f.k@gmail.com>
2023-04-18 10:40:47 -0400

    commit 74

--- src/BizHawk.Emulation.Common/SystemLookup.cs
+++ src/BizHawk.Emulation.Common/SystemLookup.cs
@@ -12,10 +12,6 @@ namespace BizHawk.Emulation.Common
 			new(VSystemID.Raw.GBA, "Gameboy Advance")
 		};
 
-		public SystemInfo this[string systemId]
-			=> _systems.FirstOrDefault(s => s.SystemId == systemId)
-			?? new SystemInfo("Unknown", "Unknown");
-
 		public IEnumerable<SystemInfo> AllSystems => _systems;
 
 		public class SystemInfo
@@ -28,6 +24,19 @@ namespace BizHawk.Emulation.Common
 
 			public string SystemId { get; }
 			public string FullName { get; }
+		}
+	}
+
+	public static class VSystemID
+	{
+		public static class Raw
+		{
+			public const string DEBUG = "DEBUG";
+			public const string GB = "GB";
+			public const string GBA = "GBA";
+			public const string GBC = "GBC";
+			public const string GBL = "GBL";
+			public const string NULL = "NULL";
 		}
 	}
 }
